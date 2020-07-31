d464ed2b559eb5820d052528130bec8bee4a5051
Serhii Khoma <srghma@gmail.com>
2020-07-31 08:45:59 +0300

    feat: add comment

--- src/CssContentToTypes.hs
+++ src/CssContentToTypes.hs
@@ -11,7 +11,6 @@ import Data.String.QQ
 import qualified Data.Text as Text
 import qualified Data.List as List
 
-
 -- [".myButton",".myButton",".myButton2","#myButton3","#myButton4",".myButton5",".classInsideClass",".classInsideClass2",,".classWithBefore2Pre",".classWithBefore2:before",".classWithBefore3:before",".classWithBefore3Post",".classInOneLine","#idInOneLine",".myButton3"]
 --
 -- from ".classWithBefore1:before" to "classWithBefore1"
@@ -30,7 +29,7 @@ extractClassesAndIds :: Text -> [Text]
 extractClassesAndIds = List.filter (\t -> Text.isPrefixOf "#" t || Text.isPrefixOf "." t) . Text.words
 
 extractClassOrId :: Text -> Maybe Text
-extractClassOrId css = join $ map (flip atMay 1) $ flip atMay 0 $ (css =~ [re|[\.#](\w+)|] :: [[Text]])
+extractClassOrId css = join $ map (flip atMay 1) $ flip atMay 0 $ (css =~ [re|[\.#]([\w\-]+)|] :: [[Text]])
 
 cssContentToTypes :: Text -> [Text]
 cssContentToTypes cssContent = ordNub $ catMaybes $ map extractClassOrId $ join $ map extractClassesAndIds $ map extractName $ join $ map collectCssBlocks $ either (const []) identity $ parseNestedBlocks cssContent
