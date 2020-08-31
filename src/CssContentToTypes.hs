fd875e18a7041c48e7b3366aaf9669a0cd63da4a
Serhii Khoma <srghma@gmail.com>
2020-08-31 13:10:16 +0300

    feat: scss

--- src/CssContentToTypes.hs
+++ src/CssContentToTypes.hs
@@ -6,23 +6,15 @@ import "protolude" Protolude
 import "text" Data.Text hiding (map)
 import Text.Regex.Base
 import Text.RE.PCRE.Text
-import Text.CSS.Parse
+import Language.Scss.Parser
 import Data.String.QQ
 import qualified Data.Text as Text
 import qualified Data.List as List
+import Text.Megaparsec (parseMaybe)
 
 -- [".myButton",".myButton",".myButton2","#myButton3","#myButton4",".myButton5",".classInsideClass",".classInsideClass2",,".classWithBefore2Pre",".classWithBefore2:before",".classWithBefore3:before",".classWithBefore3Post",".classInOneLine","#idInOneLine",".myButton3"]
---
+
 -- from ".classWithBefore1:before" to "classWithBefore1"
-
-type CssBlock = (Text, [(Text, Text)])
-
-collectCssBlocks :: NestedBlock -> [CssBlock]
-collectCssBlocks (NestedBlock mediaQuery nestedBlocks) = join $ map collectCssBlocks nestedBlocks
-collectCssBlocks (LeafBlock cssBlock) = [cssBlock]
-
-extractName :: CssBlock -> Text
-extractName = fst
 
 -- e.g. ".myButton3 > a"
 extractClassesAndIds :: Text -> [Text]
@@ -31,5 +23,10 @@ extractClassesAndIds = List.filter (\t -> Text.isPrefixOf "#" t || Text.isPrefix
 extractClassOrId :: Text -> Maybe Text
 extractClassOrId css = join $ map (flip atMay 1) $ flip atMay 0 $ (css =~ [re|[\.#]([\w\-]+)|] :: [[Text]])
 
+valueToSelectors :: Value -> [Text]
+valueToSelectors (Selector selector values) = [selector] <> (join $ map valueToSelectors values)
+valueToSelectors (AtRule name body values) = join $ map valueToSelectors values
+valueToSelectors _ = []
+
 cssContentToTypes :: Text -> [Text]
-cssContentToTypes cssContent = ordNub $ catMaybes $ map extractClassOrId $ join $ map extractClassesAndIds $ map extractName $ join $ map collectCssBlocks $ either (const []) identity $ parseNestedBlocks cssContent
+cssContentToTypes cssContent = ordNub $ catMaybes $ map extractClassOrId $ join $ map extractClassesAndIds $ either (const []) (join . map valueToSelectors) $ parse cssContent
--- test/CssContentToTypesSpec.hs
+++ test/CssContentToTypesSpec.hs
@@ -12,8 +12,6 @@ import Data.String.QQ
 
 import Text.Regex.Base
 import Text.RE.PCRE.Text
-
-import Text.CSS.Parse
 import CssContentToTypes
 import Control.Arrow
 
@@ -71,14 +69,24 @@ a > .myButton5 {
 
   a, a:visited { text-decoration: underline; }
 
-  .myButton3 > a {
+  .myButtonInsideOfPrint > a {
     color: green;
   }
+}
+
+@use "@material/button";
+
+.my_button_dense {
+  --mdc-theme-primary: #FEDBD0;
+  --mdc-theme-on-primary: #442C2E;
+
+  @include button.ink-color(#84565E);
+  @include button.density(-3);
 }
 |]
 
 spec :: Spec
 spec = do
   it "HistoryToInputsSpec" $ do
-    let (expected :: [Text]) = ["myButton","myButton2","myButton3","myButton4","myButton5","classInsideClass","classInsideClass2","classWithBefore1","classWithBefore2Pre","classWithBefore2","classWithBefore3","classWithBefore3Post","classInOneLine","idInOneLine"]
+    let (expected :: [Text]) = ["myButton","myButton2","myButton3","myButton4","myButton5","classInsideClass","classInsideClass2","classWithBefore1","classWithBefore2Pre","classWithBefore2","classWithBefore3","classWithBefore3Post","classInOneLine","idInOneLine", "myButtonInsideOfPrint", "my_button_dense"]
     cssContentToTypes cssContent `shouldBe` expected
